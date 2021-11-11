86ede1bbf7033883ab78acdfa47d99bb57682f03
nobu-hope <nobuhlelebz@gmail.com>
2021-11-11 11:28:07 +0200

    second commit

--- test/database-test.js
+++ test/database-test.js
@@ -20,40 +20,58 @@ describe('The my_fruit_basket_tests database', async function () {
         await pool.query('delete from fruit_basket');
     });
 
-    it('should be able to get the fruit_name from the fruit_basket', async function () {
+    it('should be able to add the fruit_name from to fruit_basket', async function () {
 
 
         await fruitBasket.baskets('Apple', 1, 3.00);
-
-        var getIt = await fruitBasket.getFruit('Apple')
-        assert.equal(getIt[0].fruit_name, 'Apple')
+        await fruitBasket.baskets('Banana', 1, 4.00);
+        
+        var getIt = await fruitBasket.getFruit('Apple','Banana')
+        assert.equal(getIt[0].fruit_name, 'Apple','Banana')
     });
 
     it('should be abble to update the qty', async function () {
 
         await fruitBasket.baskets('Apple', 1, 3.00);
-        await fruitBasket.updateFruit('Apple', 1, 3.00);
-        let theupdate = await fruitBasket.getFruit('Apple')
-        assert.deepEqual(2, theupdate[0].qty);
+        await fruitBasket.baskets('Banana', 1, 4.00);
+
+        await fruitBasket.updateFruit(['Apple', 1, 3.00]);
+        await fruitBasket.updateFruit(['Banana', 1, 4.00]);
+
+        let theupdate = await fruitBasket.getFruit('Apple', 'Banana')
+        assert.deepEqual(1, theupdate[0].qty);
 
 
     });
 
-    it('should be able to show the total price for a given fruit basket', async function () {
+    it('should be able to show the total price for a given fruit basket which is apple', async function () {
 
         await fruitBasket.baskets('Apple', 1, 3.00);
-        await fruitBasket.getprice('Apple', 1, 3.00)
+        
+        await fruitBasket.getprice('Apple', 1, 3.00);
         let theSum = await fruitBasket.getFruit('Apple')
-
+        
         assert.deepEqual(3.00, theSum[0].price);
 
+
     });
 
-    it('should be able to show the qty of the fruit_basket for a given fruit type', async function () {
+    it('should be able to show the total price for a given fruit basket which is Banana', async function () {
+
+        await fruitBasket.baskets('Banana', 1, 4.00);
+        await fruitBasket.getprice('Banana', 1, 4.00);
+        
+        let theSum = await fruitBasket.getFruit('Banana')
+
+        assert.deepEqual(4.00, theSum[0].price);
+
+    });
+
+    it('should be able to show the qty of the fruit_basket for a given fruit type (Apple)', async function () {
 
         await fruitBasket.baskets('Apple', 1, 3.00);
-        await fruitBasket.getqty('Apple', 1, 3.00);
-        let theQty = await fruitBasket.getFruit('Apple')
+        await fruitBasket.getqty('Banana', 1, 4.00);
+        let theQty = await fruitBasket.getFruit('Apple', 'Banana')
    
         assert.deepEqual(1, theQty[0].qty);
     });
